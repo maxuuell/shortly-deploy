5b7a63dc57232f08f4efa5f220a3ef9eb03ada76
maxuuell <maxuuell@gmail.com>
2017-01-18 19:21:59 -0800

    Third time is the charm.

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = require('./server-config.js');
 var port = 4568;
 console.log("my commit and push made it!")
 app.listen(port);
-
+console.log("this is the second test!")
 console.log('Server now listening on port ' + port);
